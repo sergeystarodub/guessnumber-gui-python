814e020819ad3953b6d0c6e7da6521f2ea0aae5a
Sergey Starodub <starodub.sergey0785@gmail.com>
2025-12-09 22:46:53 +0500

    Added an inscription to the window

--- guessnumber-GUI.py
+++ guessnumber-GUI.py
@@ -2,4 +2,10 @@ from tkinter import *
 
 window = Tk()
 window.title("Игра Угадай число")
+window.geometry('400x600')
+lblNameGame = Label(window, text='Игра Угадай число. Поиграем?')
+# Разместил lblNameGame по центру по горизонтали
+lblNameGame.grid(column=0, row=0, sticky='nsew')
+window.grid_columnconfigure(0, weight=1)
+#window.grid_rowconfigure(0, weight=1)
 window.mainloop()
